833be75997b1e2c9ca55a1076f01a44b0d134536
Nimesha Medagoda <136103215+medagoda@users.noreply.github.com>
2025-07-30 14:05:17 +0530

    UserDto added

---
+++ demo/src/main/java/com/example/demo/Dto/UserDto.java
@@ -0,0 +1,64 @@
+package com.example.demo.Dto;
+
+import com.example.demo.Enums.Role;
+import com.example.demo.Enums.Status;
+
+import java.time.LocalDateTime;
+
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime createdDate;
+    private Status status;
+    private Role roles;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Role getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Role roles) {
+        this.roles = roles;
+    }
+}
